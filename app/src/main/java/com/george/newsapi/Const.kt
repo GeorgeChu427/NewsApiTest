c4b25fa50dce19962bb2ad14d523f12fc5bdc99f
GeorgeChu <georgechu@pocket.tw>
2025-05-14 15:38:17 +0800

    [mod] 換一組API Key

--- app/src/main/java/com/george/newsapi/Const.kt
+++ app/src/main/java/com/george/newsapi/Const.kt
@@ -2,7 +2,8 @@ package com.george.newsapi
 
 
 val NEWS_API_URL = "https://newsapi.org/"
-val NEWS_API_KEY = "2beb429b00184dffa439bc67fafc9b21"
+//val NEWS_API_KEY = "2beb429b00184dffa439bc67fafc9b21"
+val NEWS_API_KEY = "889a55ddb308406e9b5b06c8c3c1dc3b"
 
 // 取 news api 每一頁的個數
 val GET_ARTICLE_PAGE_SIZE: Int = 20
